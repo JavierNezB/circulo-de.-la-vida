6972d711129a8ec9c42ea4771739ccdeed43f83b
Javier Nunez PC <javier.nez.b@gmail.com>
2024-01-28 00:50:08 -0600

    V1.0 despliegue - polarchart funcionando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import Polarchart from './Component/PolarChart';
+import React, { useState } from "react";
+import Polarchart from "./Component/PolarChart";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = useState([
+    { label: "Familia", value: 0 },
+    { label: "Salud", value: 0 },
+    { label: "Desarrollo Personal", value: 0 },
+    { label: "Carrera Profesional", value: 0 },
+    { label: "Amistades", value: 0 },
+    { label: "Pareja", value: 0 },
+    { label: "Dinero", value: 0 },
+    { label: "Diversión", value: 0 },
+  ]);
+
+  const handleInputChange = (index, value) => {
+    const newData = [...userInput];
+    newData[index].value = value;
+    setUserInput(newData);
+  };
+
   return (
     <div className="App">
-      <Polarchart/>
+      <Polarchart dataValues={userInput.map((item) => item.value)} />
+      <div className="inputs-wrapper">
+        {userInput.map((item, index) => (
+          <div key={index} className="input-container">
+            <label htmlFor={`input-${index}`}>{item.label}</label>
+            <input
+              id={`input-${index}`}
+              className="custom-input"
+              type="number"
+              value={item.value}
+              onChange={(e) => handleInputChange(index, +e.target.value)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Component/PolarChart.js
+++ src/Component/PolarChart.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Chart } from "chart.js/auto";
 
-export default function Polarchart() {
-
+export default function Polarchart({ dataValues }) {
     const chartRef = useRef(null);
     const charInstance = useRef(null);
 
@@ -10,33 +9,49 @@ export default function Polarchart() {
         if (charInstance.current) {
             charInstance.current.destroy();
         }
-        const myChartRef = chartRef.current.getContext('2d');
+        const myChartRef = chartRef.current.getContext("2d");
 
         charInstance.current = new Chart(myChartRef, {
             type: "polarArea",
             data: {
-                labels: ["Salud", "Vida", "Profesión", "Pareja", "Familia"],
-                datasets: [{
-                    data: [15, 12, 7, 3, 14],
-                    backgroundColor: [
-                        'rgb(255, 255, 132)',
-                        'rgb(255, 192, 192)',
-                        'rgb(255, 205, 86)',
-                        'rgb(255, 203, 207)',
-                        'rgb(54, 162, 235)'
-                    ]
-                }]
-            }
+                labels: [
+                    "Familia",
+                    "Salud",
+                    "Desarrollo Personal",
+                    "Carrera Profesional",
+                    "Amistades",
+                    "Pareja",
+                    "Dinero",
+                    "Diversión",
+                ],
+                datasets: [
+                    {
+                        data: dataValues || [30, 30, 30, 30, 100, 30, 30, 30],
+                        backgroundColor: [
+                            "rgb(255, 223, 186)",
+                            "rgb(255, 192, 203)",
+                            "rgb(173, 216, 230)",
+                            "rgb(240, 230, 140)",
+                            "rgb(221, 160, 221)",
+                            "rgb(144, 238, 144)",
+                            "rgb(255, 182, 193)",
+                            "rgb(175, 238, 238)",
+                        ],
+                    },
+                ],
+            },
         });
-        return()=>{
-            if(charInstance.current){
+
+        return () => {
+            if (charInstance.current) {
                 charInstance.current.destroy();
             }
-        }
-    }, [])
+        };
+    }, [dataValues]);
+
     return (
         <div>
-            <canvas ref={chartRef} style={{width:"auto", height:"auto"}}/>
+            <canvas ref={chartRef} style={{ width: "auto", height: "auto" }} />
         </div>
-    )
+    );
 }
